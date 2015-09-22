fa37e7620ea99482e431cf84c6a259a5a1400d8e
tarasgita <gittemp1@gmail.com>
2015-09-22 22:22:44 +0300

    4th commit

--- src/First.java
+++ src/First.java
@@ -4,7 +4,7 @@ public class First {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello Git");
-System.out.println("Hello Git2");
+System.out.println("Hello Git3");
 	}
 
 }
