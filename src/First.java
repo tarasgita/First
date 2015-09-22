1a22f6143fc43953a4b5fc3fec74868be0ad9dae
tarasgita <gittemp1@gmail.com>
2015-09-22 23:06:52 +0300

    from 2 eclipse

--- src/First.java
+++ src/First.java
@@ -1,11 +1,12 @@
 
 public class First {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+// TODO Auto-generated method stub
 System.out.println("Hello Git");
 System.out.println("Hello Git3");
 System.out.println("Hello Git4");
-	}
+System.out.println("Edited fro meclipse 2");
+    }
 
 }
