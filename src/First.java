aa0b0b89721b53782dd8ad334e3165467d3d3b7c
tarasgita <gittemp1@gmail.com>
2015-09-23 11:24:46 +0300

    Ready for release

--- src/First.java
+++ src/First.java
@@ -3,12 +3,7 @@ public class First {
 
     public static void main(String[] args) {
 // TODO Auto-generated method stub
-
-System.out.println("Hello Gitqqqqqqqqqqq");
-System.out.println("Hello Git3qqqqqq");
-System.out.println("Hello Git4qqqqqqq");
-System.out.println("Edited fro meclipse 2qqqqqq");
-System.out.println("Edited fro meclipse 3qqqqqqqqqq");
+    System.out.println("Ready for first release");
     }
 
 }
