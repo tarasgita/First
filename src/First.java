25e4c7a40db7f2012b63f77df713c35506437ccb
tarasgita <gittemp1@gmail.com>
2015-09-23 12:05:12 +0300

    Ready for release (final)

--- src/First.java
+++ src/First.java
@@ -3,7 +3,7 @@ public class First {
 
     public static void main(String[] args) {
 // TODO Auto-generated method stub
-    System.out.println("fggsgghg");
+    System.out.println("ready for release");
     }
 
 }
