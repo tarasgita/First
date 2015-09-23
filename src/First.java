b77224bfb64feab498fa6b3739b0bb85d3f312c9
tarasgita <gittemp1@gmail.com>
2015-09-23 12:20:17 +0300

    added new line of code

--- src/First.java
+++ src/First.java
@@ -4,6 +4,7 @@ public class First {
     public static void main(String[] args) {
 // TODO Auto-generated method stub
     System.out.println("ready for release");
+    System.out.println("this is second branch");
     }
 
 }
