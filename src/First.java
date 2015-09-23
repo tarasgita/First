35223cdeeb48e63550a6f33b4d2ac564740e4a6e
tarasgita <gittemp1@gmail.com>
2015-09-23 20:34:53 +0300

    One line of code for two branches V2

--- src/First.java
+++ src/First.java
@@ -3,7 +3,7 @@ public class First {
 
     public static void main(String[] args) {
 // TODO Auto-generated method stub
-    System.out.println("One line of code for two branches");
+    System.out.println("One line of code for two branches V2");
     }
 
 }
